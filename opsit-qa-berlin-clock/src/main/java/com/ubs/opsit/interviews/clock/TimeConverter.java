0ac7690b334e93951a7db8a945b6a9783d84fb82
c-vladys <Vladyslav.Ivashevych@s3group.com>
2015-09-29 09:24:00 +0200

    TimeConverter updated

--- opsit-qa-berlin-clock/src/main/java/com/ubs/opsit/interviews/clock/TimeConverter.java
+++ opsit-qa-berlin-clock/src/main/java/com/ubs/opsit/interviews/clock/TimeConverter.java
@@ -1,22 +1,35 @@
 package com.ubs.opsit.interviews.clock;
 
 class TimeConverter {
-	
+
 	/*
-	 * Parses a string object, e.g. time = "13:17:01" and extracts Hours=13, Minutes=17 and Seconds=1 
+	 * Parses a string object, e.g. time = "13:17:01" and extracts Hours=13,
+	 * Minutes=17 and Seconds=1
 	 */
-	
-	private String time;
+
+	private int hours;
+	private int minutes;
+	private int seconds;
+
 	public TimeConverter(String time) {
-		this.time = time;
+		try {
+			hours = Integer.valueOf(time.split(":")[0]);
+			minutes = Integer.valueOf(time.split(":")[1]);
+			seconds = Integer.valueOf(time.split(":")[2]);
+		} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+			throw new IllegalArgumentException(e + ". Time should be in format hh:mm:ss.");
+		}
 	}
+
 	public int getHour() {
-		return Integer.valueOf(time.split(":")[0]);
+		return hours;
 	}
+
 	public int getMinute() {
-		return Integer.valueOf(time.split(":")[1]);	
+		return minutes;
 	}
+
 	public int getSecond() {
-		return Integer.valueOf(time.split(":")[2]);	
+		return seconds;
 	}
 }
